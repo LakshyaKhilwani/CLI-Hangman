86eb15186c9c1e8b7b662fdff82d920c09029360
Rasesh Shetty <rasesh.udayshetty@gmail.com>
2021-02-10 14:52:48 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,195 @@
+import random
+from os import system,name 
+with open("words.txt") as f:
+    words_list=f.read().split(" ")
+
+word=random.choice(words_list)
+
+def clear(): 
+  
+    # for windows 
+    if name == 'nt': 
+        _ = system('cls') 
+  
+    # for mac and linux(here, os.name is 'posix') 
+    else: 
+        _ = system('clear') 
+HANGMAN = (
+    """
+ ------
+|     |
+|
+|
+|
+|
+|
+|
+----------
+"""
+    ,
+"""
+ ------
+|     |
+|     0
+|
+|
+|
+|
+|
+----------
+"""
+   ,
+"""
+ ------
+|     |
+|     0
+|     +
+|
+|
+|
+|
+----------
+"""
+    ,
+"""
+ ------
+|     |
+|     0
+|    -+
+|
+|
+|
+|
+----------
+"""
+    ,
+"""
+ ------
+|     |
+|     0
+|    -+-
+|
+|
+|
+|
+----------
+"""
+    ,
+"""
+ ------
+|     |
+|     0
+|   /-+-
+|
+|
+|
+|
+----------
+"""
+    ,
+"""
+ ------
+|     |
+|     0
+|   /-+-\\
+|
+|
+|
+|
+----------
+"""
+    ,
+"""
+ ------
+|     |
+|     0
+|   /-+-\\
+|     |
+|
+|
+|
+----------
+"""
+    ,
+"""
+ ------
+|     |
+|     0
+|   /-+-\\
+|     |
+|     |
+|
+|
+----------
+"""
+    ,
+"""
+ ------
+|     |
+|     0
+|   /-+-\\
+|     |
+|     |
+|    /
+|   /
+----------
+"""
+    ,
+"""
+ ------
+|     |
+|     0
+|   /-+-\\
+|     |
+|     |
+|    / \\
+|   /   \\
+----------
+"""
+)
+
+attemptLeft = len(HANGMAN)-1
+
+correctGuesses=[]
+
+def get_hangman():
+    return HANGMAN[-1-attemptLeft]
+
+def get_word_state():
+    res=""
+    for i in word:
+        if i in correctGuesses:
+            res+=i
+        else:
+            res+='_ '
+    return res
+
+def get_input():
+    global attemptLeft
+    guess=input("Guess your Letter: ")[0]
+    if guess in word:
+        correctGuesses.append(guess)
+        print("You Guessed it correctly...")
+        return True
+    else:
+        attemptLeft-=1
+        print("You Couldn't guess the word..")
+        return False
+
+while "_ " in get_word_state() and attemptLeft:
+    clear()
+    print(get_hangman())
+    print("Word:",get_word_state())
+    correctGuess=get_input()
+
+clear()
+print(get_hangman())
+print("Word:",get_word_state())
+
+if attemptLeft:
+    print("Congratulations, You guessed the word ",word)
+else:
+    print("The word is:",word)
+
+print("Press enter to exit..")
+input()
